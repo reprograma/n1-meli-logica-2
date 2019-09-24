182734d3c927114d60ee78915a53d6cf2df62abf
danikarasawa <danikarasawa@gmail.com>
2019-09-23 22:24:54 -0300

    new itens

--- 1_tipos_de_dados/exercicio.js
+++ 1_tipos_de_dados/exercicio.js
@@ -1,1 +1,27 @@
-//resposta aqui
+// A loja Lumelância convidou você para desenvolver um script simples para seus clientes.
+
+// Você criará um cartão fidelidade na loja Lumelância e deverá seguir os passos abaixo:
+
+// - Crie variaveis para o guardar o nome, idade, se é a primeira vez e uma lista produtos favoritos do cliente
+// - Atribua valores a essas variaveis
+// - Crie um objeto que irá receber os valores dessa variaveis e o imprima o objeto no console
+
+// let clientName = ['Dani', 'Carla', 'Sofia', 'Vanessa'];
+// let clientAge = ['30', '29', '42', '19'];
+// let clientFirstTime = ['S', 'N', 'N', 'S'];
+// let clientFavoriteItem = ['Sabonete/Xampu', 'Hidratante/Esfoliante', 'Xampu/Condicionador', 'Esfoliante/Protetor Solar'];
+
+// console.log(clientName[1], clientAge[1]);
+
+let name = 'Dani';
+let age = 40;
+let firstTime = true; // melhor quando você precisa dessa info em vários idiomas, mais fácil de comparar
+let favoriteItens =  ['Music', 'Films', 'Comics'];
+let client = {
+    quem: name,
+    idade: age,
+    primeira_vez: firstTime,
+    favoritos: favoriteItens,
+};
+
+console.log(client);
--- 2_op_ar/exercicio.js
+++ 2_op_ar/exercicio.js
@@ -1,1 +1,37 @@
-//resposta aqui
+// A loja Lumelância precisa de uma nova função no seu sistema para calcular um desconto nos seus produtos.
+
+// Você deverá seguir os seguintes passos:
+
+// - Crie uma variável que irá guardar o array com nome e valor dos produtos
+// - Crie uma variável 'posicao' para guardar o produto escolhido pelo usuário
+// - Crie uma variável 'porcentagem' que irá guardar qual porcentagem o usuário digitou
+// - Procure no array qual produto foi escolhido pelo usuário
+// - Calcule o desconto do produto
+// - Printe na tela a seguinte frase: "Produto xxx com desconto é R$ xxx"
+
+// Dica: você pode usar a função .toFixed(digits) para limitar o numero de casas decimais do desconto.
+
+// let r = require('readline-sync');
+
+// let product = ['xampu', 'condicionador', 'hidratante'];
+// let productPrice = [59.90, 65.00, 74.90];
+// let clientChoice = product&&productPrice.indexOf.parseInt(r.question('Digite o número do produto, por favor'));
+// let promo = parseInt.tofixed(2) = (r.question('Digite o valor do desconto, por favor'));
+
+// let discont = productPrice*(promo/100);
+
+// console.log = (`Produto ${clientChoice} com desconto de R$ ${discont}`);
+
+let product = '';
+let price = '';
+let productList = [{product: 'xampu', price: 59.90}, 
+                   {product: 'condicionador', price: 65.00}, 
+                   {product: 'hidratante', price: 74.90}];
+
+let clientChoice = productList[parseInt(r.question('Digite o número do produto, por favor'))];
+let promo = parseInt(r.question('Digite o valor do desconto, por favor'));
+
+let discont = productPrice*(promo/100);
+
+console.log = (`Produto ${clientChoice} com desconto de R$ ${discont}`);
+
