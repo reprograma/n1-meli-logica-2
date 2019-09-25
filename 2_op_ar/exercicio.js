f270892c2f1003465d24a317297f0eaf38705421
karolcosta <karolinycosta@yahoo.com.br>
2019-09-25 20:41:12 -0300

    exercicios

--- 2_op_ar/exercicio.js
+++ 2_op_ar/exercicio.js
@@ -1,1 +1,10 @@
 //resposta aqui
+
+const lista = [
+    {nome: "Banana", valor: 2.50},
+    {nome: "maça", valor: 3.50},
+    {nome: "melancia", valor: 4.50}
+
+]
+
+var 
--- 3_estrutura/exercicio.js
+++ 3_estrutura/exercicio.js
@@ -1,1 +1,46 @@
 //resposta aqui
+
+var r = require('readline-sync');
+
+var produtos = [
+    {
+        nome = 'Banana'
+        tempoDeValidade = 3,
+        congelado = true
+    }
+    {
+        nome = 'salsicha'
+        tempoDeValidade = 10,
+        congelado = true
+    }
+    {
+        nome = 'azeitona'
+        tempoDeValidade = 30,
+        congelado = true
+    }
+]
+
+var produtos = parseInt(r.question('Escolha um produto entre 0 e 3: '));
+var tempoDeValidadeDoProduto = parseInt(r.question('Digite quantos dias faltam para vencer: '));
+var produtos = produtos[posicao];
+var tempoDeValidade = (dias * 100) /produtos.tempoDeValidadeDoProduto;
+
+console.log(porcentagem);
+
+if 
+    (tempoDeValidade <= 2)
+    {
+        console.log('Descartar produto, muito proximo de vencer');
+    }
+else if
+    (empoDeValidade <= 10 && ! produtos.congelado )
+    {
+        console.log ('Desconto!  Produto não congelado e próximo da validade.');
+    }
+else if 
+    (tempoDeValidade <= 5 && produtos.congelado)
+    {
+        console.log ('Desconto! Produto congelado e próximo da validade.');
+    }
+else
+    console.log('Produto longe do vencimento.');
