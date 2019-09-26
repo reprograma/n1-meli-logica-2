29ff098ee6f87ad4cdbaacfcce94118cc8be77f9
Luma Marigo <lumamarigo@gmail.com>
2019-09-26 20:04:12 -0300

    arquivo varletconst

---
+++ varletconst.js
@@ -0,0 +1,45 @@
+// var idade = 30;
+// console.log("Idade: " + idade);
+
+// function imprimeIdade(){
+//     var idade = 30;
+//     console.log("Idade: " + idade);
+// }
+
+// imprimeIdade();
+
+// function imprimeIdade(){
+//     var idade = 30;
+// }
+
+// console.log("Idade: " + idade);
+
+// imprimeIdade();
+
+// function imprimeIdade(){
+//     if(true){
+//         var idade = 30;
+//         console.log("Idade dentro do if: " + idade);
+//     }
+
+//     console.log("idade fora do if:" + idade);
+// }
+// imprimeIdade();
+
+// function imprimeIdade(){
+//     if(true){
+//         let idade = 30;
+//         console.log("idade dentro do if" + idade);
+//     }
+//     console.log("idade fora do if" + idade);
+// }
+
+// imprimeIdade();
+
+const pessoa = {
+    nome: 'luma',
+    idade: 30
+};
+
+pessoa.nome = 'xtina';
+console.log("nome" + pessoa.nome);
